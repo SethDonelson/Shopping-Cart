669031c898a7dc5b29818567f9701bba3ccc47f3
Seth Donelson <155389209+SethDonelson@users.noreply.github.com>
2024-11-04 20:50:11 -0500

    added products files

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,28 @@
 import { FunctionComponent } from 'react'
+import { Link } from 'react-router-dom'
+
 import classes from  './header.module.scss'  //import styling
+
+
 
 export const Header: FunctionComponent = () => {
     
-    return (
-        <header className={classes.header}>
-            <div>
-                <Link to='/'>
-                <img src={logo} className={classes.logo} alt="Shopping Cart Application" />
-                </Link>
-            </div>
-            <div>
-                <CartWidget productsCount={productsCount} />
-            </div>
-        </header>
+
+
+
+     return (
+    <header className={classes.header}>
+      <div>
+
+        <span>this is a test for the header</span>
+
+        <Link to="/">
+          <img src={logo} className={classes.logo} alt="Shopping Cart Application" />
+        </Link>
+      </div>
+      <div>
+        <CartWidget productsCount={productsCount} />
+      </div>
+    </header>
     )
 }
